77c5ab34ffbf075835738520bbee486421fd81ec
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2024-02-02 18:46:37 +0200

    added a modal controlles to handle submit and exit

--- src/components/modals/modal/Modal.tsx
+++ src/components/modals/modal/Modal.tsx
@@ -1,20 +1,41 @@
 import { useState, type FunctionComponent, type ReactNode } from "react";
 import { createPortal } from "react-dom";
 import "./Modal.scss";
+import { LiaSave } from "react-icons/lia";
+import { IoMdClose } from "react-icons/io";
+
 interface ModalProps {
   children: ReactNode;
+  onSubmit: () => void;
 }
 
-const Modal: FunctionComponent<ModalProps> = ({ children }) => {
+const Modal: FunctionComponent<ModalProps> = ({ children, onSubmit }) => {
   const [isOpen, setIsOpen] = useState<boolean>(true); // the defult should be false
 
   const handleCloseModal = () => {
     setIsOpen(false);
   };
+
   return createPortal(
     <div className={`modal ${!isOpen && "closed"}`}>
       <div onClick={handleCloseModal} className="modal-overlay"></div>
-      <div className="modal-content">{children}</div>
+      <div className="modal-content">
+        {children}
+        <div className="modal-controlles">
+          <button className="modal-btn submit" onClick={onSubmit}>
+            <i>
+              <LiaSave />
+            </i>
+            Save
+          </button>
+          <button className="modal-btn" onClick={handleCloseModal}>
+            <i>
+              <IoMdClose />
+            </i>
+            Cancle
+          </button>
+        </div>
+      </div>
     </div>,
     document.getElementById("modal")!
   );
