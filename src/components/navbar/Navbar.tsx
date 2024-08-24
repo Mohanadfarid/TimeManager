9f99d246355f4b484555c65e7790c16ee4c6e7b1
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2024-08-24 21:12:13 +0300

    collapsed navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,23 +5,40 @@ import { CgSandClock } from "react-icons/cg";
 import { GoBell } from "react-icons/go";
 import { RiTimerLine } from "react-icons/ri";
 import { SlCalender } from "react-icons/sl";
+import { IoMenuSharp } from "react-icons/io5";
+import { FaClock } from "react-icons/fa";
+
 interface NavbarProps {}
 
 const Navbar: FunctionComponent<NavbarProps> = () => {
   return (
     <nav className="navbar">
+      <div className={"navbar__link"}>
+        <IoMenuSharp className={"navbar__icon hamburger-menu"} />
+        <span className={"navbar__title"}>
+        <FaClock className={'navbar__icon logo-icon'} />
+          TimeManager
+        </span>
+      </div>
+
       <NavLink className={"navbar__link"} to={"/"}>
-        <CgSandClock className="navbar__icon" /> timers
+        <CgSandClock className="navbar__icon" />{" "}
+        <span className={"navbar__title"}>timers</span>
       </NavLink>
+
       <NavLink className={"navbar__link"} to={"/alarm"}>
-        <GoBell className="navbar__icon"/> alarm
+        <GoBell className="navbar__icon" />{" "}
+        <span className={"navbar__title"}>alarm</span>
       </NavLink>
+
       <NavLink className={"navbar__link"} to={"/stopwatch"}>
-        <RiTimerLine className="navbar__icon"/>
-        stopwatch
+        <RiTimerLine className="navbar__icon" />
+        <span className={"navbar__title"}>stopwatch</span>
       </NavLink>
+
       <NavLink className={"navbar__link"} to={"/calender"}>
-        <SlCalender className="navbar__icon"/> calender
+        <SlCalender className="navbar__icon" />{" "}
+        <span className={"navbar__title"}>calender</span>
       </NavLink>
     </nav>
   );
